2dca57b678b802d568bfad82e8dd02ba43fe24d1
Vincent Zhu <vzhu@incomm.com>
2022-02-16 13:05:00 -0500

    Got the stork to appear on click.

---
+++ script.js
@@ -0,0 +1,4 @@
+function loadAnimation(){
+    document.getElementById("stork").style.display="block";
+    document.getElementById("textOne").innerText=document.getElementById("inputOne").value;
+}
